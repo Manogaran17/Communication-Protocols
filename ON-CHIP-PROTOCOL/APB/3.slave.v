e957aa5eea7e998315f8064b0fa1992ea283059a
Manogaran17 <manogaran2667@gmail.com>
2026-03-08 23:59:46 +0530

    Implement APB slave module with memory operations

--- ON-CHIP-PROTOCOL/APB/3.slave.v
+++ ON-CHIP-PROTOCOL/APB/3.slave.v
@@ -1,1 +1,44 @@
 
+
+  module apb_slave(
+    
+    input pclk,
+    input prst,
+    input psel,
+    input penable,
+    input pwrite,
+    input[8:0]paddr,
+    input [7:0]pwdata,
+    output reg[7:0]prdata,
+    output reg pready
+  );
+    
+    reg [7:0]mem[0:127];
+    
+    integer i;
+    
+    always@(posedge pclk or negedge prst)
+      begin
+        if(!prst)
+          begin
+          pready<=0;
+          prdata<=0;
+            
+            for(i=0;i<128;i=i+1)
+              mem[i]<=0;
+          end 
+        else
+          begin
+            pready<=0;
+            
+            if(psel&&penable)
+              begin
+                pready<=1;
+                if(pwrite)
+                  mem[paddr[6:0]]<=pwdata;
+                else 
+                  prdata<=mem[paddr[6:0]];
+              end 
+          end
+      end 
+  endmodule  
