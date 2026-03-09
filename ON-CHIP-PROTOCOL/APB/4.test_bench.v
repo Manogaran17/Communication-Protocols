8f141e1892c99a10d97c86f86ecf2ed7f9d4ef7b
Manogaran17 <manogaran2667@gmail.com>
2026-03-09 20:04:55 +0530

    Enhance testbench with more write operations
    Added additional write operations to the testbench for various addresses and data values.

--- ON-CHIP-PROTOCOL/APB/4.test_bench.v
+++ ON-CHIP-PROTOCOL/APB/4.test_bench.v
@@ -1,14 +1,19 @@
 
+
 module tb_apb;
+
 reg pclk;
 reg prst;
 reg transfer;
 reg pwrite_in;
+
 reg [7:0] write_data;
 reg [8:0] addr;
+
 wire [7:0] read_data;
 
 apb_top DUT(
+
 .pclk(pclk),
 .prst(prst),
 .transfer(transfer),
@@ -16,14 +21,13 @@ apb_top DUT(
 .write_data(write_data),
 .addr(addr),
 .read_data(read_data)
-);
 
+);
 initial
 begin
     pclk = 1;
     forever #5 pclk = ~pclk;
 end
-
 initial
 begin
 prst = 0;
@@ -41,6 +45,7 @@ transfer = 1;
 #10;
 transfer = 0;
 #40;
+    
 pwrite_in = 1;
 addr = 9'b010000100;
 write_data = 8'hBB;
@@ -48,6 +53,7 @@ transfer = 1;
 #10;
 transfer = 0;
 #40;
+    
 pwrite_in = 1;
 addr = 9'b100000110;
 write_data = 8'hCC;
@@ -55,18 +61,44 @@ transfer = 1;
 #10;
 transfer = 0;
 #40;
+    
 pwrite_in = 1;
 addr = 9'b010000011;
 write_data = 8'hDD;
 transfer = 1;
 #10;
 transfer = 0;
+  #40;
+    
+pwrite_in = 0;
+addr = 9'b000000010;
+transfer = 1;
+#10;
+transfer = 0;
+#40;
+    
+ pwrite_in = 0;
+addr = 9'b010000100;
+transfer = 1;
+#10;
+transfer = 0;
+  #40;
+  
+pwrite_in = 0;
+addr = 9'b100000110;;
+transfer = 1;
+#10;
+transfer = 0;
+    
 #80;
 $finish;
-end  
+
+end
   initial 
     begin
       $dumpfile("apb.vcd");
       $dumpvars(1,tb_apb);
     end 
 endmodule
+
+
